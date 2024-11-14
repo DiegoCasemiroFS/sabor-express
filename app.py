777cd92ee5f7016f4d0e143dd43af5ed401f49cb
Diego Casemiro <dcfs1998@gmail.com>
2024-11-14 14:43:55 -0300

    adicionando funcao finalizar_app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print('Sabor Express\n')
 
 print('1. Cadastrar Restaurante')
@@ -7,6 +9,10 @@ print('4. Sair\n')
 
 opcao_escolhida = int(input('Escolha uma opção: '))
 
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando app\n')
+
 if opcao_escolhida == 1:
     print('Cadastrar Restaurante')
 elif opcao_escolhida == 2:
@@ -14,4 +20,4 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print('Ativar Restaurante')
 else:
-    print('Sair')
+    finalizar_app()
