7568934231fd9980dd8d1ea9b5b37b3635ccde81
Diego Casemiro <dcfs1998@gmail.com>
2024-11-14 14:01:02 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar Restaurante')
+else:
+    print('Sair')
